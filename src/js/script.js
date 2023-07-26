49552e0403ba00fd90f8a000d18b30c984fd7300
LuisComZ <luizmay2@gmail.com>
2023-07-26 14:53:10 -0300

    key movement and grid

--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,7 @@ const canvas = document.querySelector("canvas")
 const ctx = canvas.getContext("2d")
 
 const size = 30
-const snake = [
-  {x: 200, y: 200},
-  {x: 230, y: 200},
-]
+const snake = [{ x: 300, y: 300 }]
 
 let direction
 let loopId
@@ -19,7 +16,7 @@ function drawSnake() {
 
     ctx.fillRect(position.x, position.y, size, size)
   })
-} 
+}
 
 function moveSnake() {
   if (!direction) return
@@ -27,22 +24,40 @@ function moveSnake() {
   const head = snake[snake.length - 1]
 
   if (direction === "right") {
-    snake.push({x: head.x + size, y: head.y})
+    snake.push({ x: head.x + size, y: head.y })
   } else if (direction === "left") {
-    snake.push({x: head.x - size, y: head.y})
+    snake.push({ x: head.x - size, y: head.y })
   } else if (direction === "down") {
-    snake.push({x: head.x, y: head.y + size})
+    snake.push({ x: head.x, y: head.y + size })
   } else if (direction === "up") {
-    snake.push({x: head.x, y: head.y - size})
+    snake.push({ x: head.x, y: head.y - size })
   }
 
   snake.shift()
+}
+
+function drawGrid() {
+  ctx.lineWidth = 1
+  ctx.strokeStyle = "#191919"
+
+  for (let i = 30; i < canvas.width; i += 30) {
+    ctx.beginPath()
+    ctx.lineTo(i, 0)
+    ctx.lineTo(i, 600)
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.lineTo(0, i)
+    ctx.lineTo(600, i)
+    ctx.stroke()
+  }
 }
 
 function gameLoop() {
   clearInterval(loopId)
 
   ctx.clearRect(0, 0, 600, 600)
+  drawGrid()
   moveSnake()
   drawSnake()
 
@@ -52,3 +67,31 @@ function gameLoop() {
 }
 
 gameLoop()
+
+document.addEventListener("keydown", function ({ key }) {
+  if (
+    (key == "ArrowRight" && direction != "left") ||
+    (key == "d" && direction != "left") ||
+    (key == "D" && direction != "left")
+  ) {
+    direction = "right"
+  } else if (
+    (key == "ArrowLeft" && direction != "right") ||
+    (key == "a" && direction != "right") ||
+    (key == "A" && direction != "right")
+  ) {
+    direction = "left"
+  } else if (
+    (key == "ArrowDown" && direction != "up") ||
+    (key == "s" && direction != "up") ||
+    (key == "S" && direction != "up")
+  ) {
+    direction = "down"
+  } else if (
+    (key == "ArrowUp" && direction != "down") ||
+    (key == "w" && direction != "down") ||
+    (key == "W" && direction != "down")
+  ) {
+    direction = "up"
+  }
+})
